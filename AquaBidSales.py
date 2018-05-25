2c894bf0a32a82f17db3129a68b106f5755ab02c
HalmonLui <halmonlui@gmail.com>
2018-05-25 11:17:35 -0400

    Changed lists to dictionary of lists for better organization.
    Prints listing number, item name and seller name.
    
    Still need to implement pricing.

--- AquaBidSales.py
+++ AquaBidSales.py
@@ -1,34 +1,46 @@
 import urllib
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-my_url = 'http://www.aquabid.com/cgi-bin/auction/auction.cgi?liveplantsm'
+
 
 # opening up connection, grabbing page
-req = urllib.request.Request(my_url, headers={'User-Agent' : "Magic Browser"})
-uClient = uReq(req)
-page_html = uClient.read()
-uClient.close()
-
-# put everything inside page_soup
-page_soup = soup(page_html, "html.parser")
+def getWebpage(my_url):
+    req = urllib.request.Request(my_url, headers={'User-Agent' : "Magic Browser"})
+    uClient = uReq(req)
+    page_html = uClient.read()
+    uClient.close()
+    return soup(page_html, "html.parser")
 
 
 # fetch the seller
-sellerList = []
-for link in page_soup.find_all('a'):
-    seller = (link.get('href'))
-    if seller is None:
-        continue
-    elif "viewseller" in seller:
-        sellerList.append(seller.split("viewseller&",1)[1])
+def getSeller(page_soup):
+    for link in page_soup.find_all('a'):
+        seller = (link.get('href'))
+        if seller is None:
+            continue
+        elif "viewseller" in seller:
+            fullListing.setdefault('seller', []).append(seller.split("viewseller&",1)[1])
 
 
 # fetch the listing items
-itemList = []
-for num, link in enumerate(page_soup.find_all('a')):
-    title = (link.getText('href'))
-    itemList.append(title)
-    if itemList[num] in sellerList:
-        print ("Item: " + itemList[num-1])
-        print ("Seller: " + itemList[num])
-        print ()
+def getItem(page_soup):
+    itemList = []
+    for num, link in enumerate(page_soup.find_all('a')):
+        title = (link.getText('href'))
+        title = title.replace('href', '')
+        itemList.append(title)
+        if itemList[num] in fullListing['seller']:
+            fullListing.setdefault('item', []).append(itemList[num-1])
+
+
+# output information
+fullListing = {}
+
+page_soup = getWebpage('http://www.aquabid.com/cgi-bin/auction/auction.cgi?liveplantsm')
+getSeller(page_soup)
+getItem(page_soup)
+for num, item in enumerate(fullListing['item']):
+    print ("Listing #" + str(num+1) + ":")
+    print ("Item:    " + fullListing['item'][num])
+    print ("Seller:  " + fullListing['seller'][num])
+    print ()
